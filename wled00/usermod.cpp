c8cd8c86078b13af6e835ca3ca1bfd9960c6003a
nicedevil007 <17103076+nicedevil007@users.noreply.github.com>
2021-01-24 09:27:30 +0100

    new topic

--- wled00/usermod.cpp
+++ wled00/usermod.cpp
@@ -16,7 +16,7 @@
 // PIR sensor pin
 const int MOTION_PIN = 4; // D2
  // PIR MQTT topic
-const char MQTT_TOPIC_PIR[] = "/motion";
+const char MQTT_TOPIC_PIR[] = "/sensor/motion";
 
 int prevState = LOW;
 
@@ -25,8 +25,8 @@ const int DHT_PIN = 5; // D1
 #define DHTtype DHT22
 DHT dht(DHT_PIN, DHTtype);
  // DHT MQTT topic
-const char MQTT_TOPIC_TEMP[] = "/temp";
-const char MQTT_TOPIC_HUM[] = "/hum";
+const char MQTT_TOPIC_TEMP[] = "/sensor/temp";
+const char MQTT_TOPIC_HUM[] = "/sensor/hum";
 
 
 const long UPDATE_MS = 60000; // Upper threshold between mqtt messages
